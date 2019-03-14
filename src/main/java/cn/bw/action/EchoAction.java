13717a8c912f116b6b58cf101398b27769015cdb
liyue <liyue@qq.com>
2019-03-14 18:00:37 +0800

    pull some codes

--- src/main/java/cn/bw/action/EchoAction.java
+++ src/main/java/cn/bw/action/EchoAction.java
@@ -1,5 +1,13 @@
 package cn.bw.action;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
 @SuppressWarnings("serial")
@@ -17,7 +25,27 @@ public class EchoAction extends ActionSupport{
 	
 	@Override
 	public String execute() throws Exception{
+		//内置对象
+//		HttpServletRequest request = ServletActionContext.getRequest();
+//		HttpServletResponse response = ServletActionContext.getResponse();
+//		ServletContext application = ServletActionContext.getServletContext();
+//		HttpSession session = request.getSession() ;
+//		
+//		String contextPath = request.getContextPath();
+//		String contentType = request.getContentType();
+//		String realPath = application.getRealPath("/");
+//		String sessionId = session.getId();
+//		System.out.println(contextPath);
+//		System.out.println(contentType);
+//		System.out.println(realPath);
+//		System.out.println(sessionId);
+		
+		System.out.println(super.getText("pages.success"));
+		System.out.println(super.getText("info.msg", new String[] {"你好","我是","liyue"}));
+		
 		return "echo.page";
+//		Action.SUCCESS
+//		Action.ERROR
 	}
 	
 }
